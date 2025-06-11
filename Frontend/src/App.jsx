06de9427677abb5bee4418fabeb190c4f741baef
k228719 <k228719@nu.edu.pk>
2025-06-11 16:25:07 +0500

    Commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -20,6 +20,7 @@ function App() {
   const [review, setReview] = useState('')
   async function reviewCode(){
     const response=await axios.post(`${import.meta.env.REACT_APP_API_URL}/ai/get-review`,{code})
+    console.log('API URL:', import.meta.env.VITE_API_URL)
     setReview(response.data)
   }
 
